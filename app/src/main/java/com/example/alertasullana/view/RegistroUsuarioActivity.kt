48760e48f8f3f1614a9470020e65919405e61533
peteragurto <130622718+peteragurto@users.noreply.github.com>
2023-10-30 02:09:32 -0500

    Servicio de verificación 1.0

---
+++ app/src/main/java/com/example/alertasullana/view/RegistroUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alertasullana.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alertasullana.R
+
+class RegistroUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuario)
+    }
+}
