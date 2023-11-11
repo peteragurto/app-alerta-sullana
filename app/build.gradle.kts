ff4ca1a287d1b59ed2c87d880448719c2077fe68
Jblanch16 <149317451+Jblanch16@users.noreply.github.com>
2023-11-11 13:02:21 -0500

    fragmento google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 }
--- app/src/main/java/com/example/alertasullana/ui/view/principal/MainActivity.kt
+++ app/src/main/java/com/example/alertasullana/ui/view/principal/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.alertasullana.ui.view.principal
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.example.alertasullana.R
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
