ede78816475f0b491cbf2f057a2f774f4af1367d
peteragurto <130622718+peteragurto@users.noreply.github.com>
2023-11-12 22:19:16 -0500

    Revert "dependencias"
    This reverts commit e6a2c726a6cb33988acede8091753c5d628186de.

--- app/src/main/java/com/example/alertasullana/ui/view/principal/MainActivity.kt
+++ app/src/main/java/com/example/alertasullana/ui/view/principal/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.alertasullana.ui.view.principal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.example.alertasullana.R
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/alertasullana/ui/view/registro/RegistroUsuarioActivity.kt
+++ app/src/main/java/com/example/alertasullana/ui/view/registro/RegistroUsuarioActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.alertasullana.R
 
 class RegistroUsuarioActivity : AppCompatActivity() {
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
