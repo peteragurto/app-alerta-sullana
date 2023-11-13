489cae11ef30b27d2a411d2d37f52aa3ed994dbf
peteragurto <130622718+peteragurto@users.noreply.github.com>
2023-11-12 22:01:40 -0500

    Revert "Merge branch 'main' of https://github.com/peteragurto/app-alerta-sullana"
    This reverts commit 1e0e95dd76a548d486b465b5cf0ef699a9a702a3, reversing
    changes made to 8c49f3d3f6fd8ecd7e0df3bd8ff31ef469d372d2.

--- app/src/main/java/com/example/alertasullana/ui/view/registro/RegistroUsuarioActivity.kt
+++ app/src/main/java/com/example/alertasullana/ui/view/registro/RegistroUsuarioActivity.kt
@@ -1,61 +1,12 @@
 package com.example.alertasullana.ui.view.registro
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alertasullana.R
-import com.example.alertasullana.ui.view.principal.MainActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.material.button.MaterialButton
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 
 class RegistroUsuarioActivity : AppCompatActivity() {
-    private lateinit var materialButton: MaterialButton
-    private lateinit var client: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
-        materialButton = findViewById<MaterialButton>(R.id.button_login_google)
-        val  options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        client = GoogleSignIn.getClient(this,options)
-        materialButton.setOnClickListener {
-            val intent = client.signInIntent
-            startActivityForResult(intent,10001)
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==10001){
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            val account = task.getResult(ApiException::class.java)
-            val credential = GoogleAuthProvider.getCredential(account.idToken,null)
-            FirebaseAuth.getInstance().signInWithCredential(credential)
-                .addOnCompleteListener{task->
-                    if(task.isSuccessful){
-
-                        val i  = Intent(this, MainActivity::class.java)
-                        startActivity(i)
-
-                    }else{
-                        Toast.makeText(this,task.exception?.message, Toast.LENGTH_SHORT).show()
-                    }
-
-                }
-        }
-    }
-    override fun onStart() {
-        super.onStart()
-        if(FirebaseAuth.getInstance().currentUser != null){
-            val i  = Intent(this, MainActivity::class.java)
-            startActivity(i)
-        }
     }
 }
