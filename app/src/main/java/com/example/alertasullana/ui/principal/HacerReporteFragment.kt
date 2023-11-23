3c3e2b180e60f9b4449c50eaa8c695c2ec360197
peteragurto <130622718+peteragurto@users.noreply.github.com>
2023-11-23 11:49:51 -0500

    Primero diseño de HacerReporteFragment y cambios extra

--- app/src/main/java/com/example/alertasullana/ui/principal/HacerReporteFragment.kt
+++ app/src/main/java/com/example/alertasullana/ui/principal/HacerReporteFragment.kt
@@ -1,22 +1,13 @@
 package com.example.alertasullana.ui.principal
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.alertasullana.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HacerReporteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HacerReporteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -24,10 +15,7 @@ class HacerReporteFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -52,8 +40,7 @@ class HacerReporteFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             HacerReporteFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
