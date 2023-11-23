f9279d24f7cce11e6da9f50b36ccb09ab523c24c
peteragurto <130622718+peteragurto@users.noreply.github.com>
2023-11-22 23:58:36 -0500

    Comentando el fragmento Home para mapa

--- app/src/main/java/com/example/alertasullana/ui/principal/HomeFragment.kt
+++ app/src/main/java/com/example/alertasullana/ui/principal/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.example.alertasullana.ui.principal
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -32,55 +34,73 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        // Inicializa el fragmento del mapa
         createFragment()
+        // Establece el color de fondo del fragmento
         view.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.colorBackgroundDark))
         return view
     }
 
+    // Función para inicializar el fragmento del mapa
     private fun createFragment() {
         val mapFragment : SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    // Callback cuando el mapa está listo para su uso
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        // Crea y muestra un marcador en una ubicación específica
         createMarker()
+        // Activa la funcionalidad de ubicación en tiempo real
         enableLocation()
+        // Configura escuchadores de eventos en el mapa
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMapClickListener(this)
+        // Aplica un estilo personalizado al mapa
         map.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.estilo_oscuro))
     }
 
+    // Crea un marcador en una ubicación específica
     private fun createMarker() {
         val coordinates = LatLng(-4.909004, -80.694229)
         val marker: MarkerOptions = MarkerOptions().position(coordinates).title("Sullana")
         map.addMarker(marker)
+        // Centra la cámara en la ubicación del marcador con un zoom específico
         map.animateCamera(
             CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
             2000, null
         )
     }
 
+    // Verifica si se han concedido los permisos de ubicación
     private fun isLocationPermissionGranted() =
         ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
+    // Activa la funcionalidad de ubicación en tiempo real
     private fun enableLocation() {
         if (!::map.isInitialized) return
+        // Verifica si se han concedido los permisos de ubicación
         if (isLocationPermissionGranted()) {
+            // Habilita la capa de ubicación en tiempo real del mapa
             map.isMyLocationEnabled = true
         } else {
+            // Si no se han concedido permisos, solicita al usuario que los conceda
             requestLocationPermission()
         }
     }
 
+    // Solicita permisos de ubicación al usuario
     private fun requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 requireActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION
             )
         ) {
-            Toast.makeText(requireContext(), "Ve ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            // Muestra un mensaje explicativo al usuario sobre la necesidad de permisos
+            Toast.makeText(requireContext(), "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
         } else {
+            // Si no se deben mostrar explicaciones, solicita permisos directamente
             ActivityCompat.requestPermissions(
                 requireActivity(),
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -89,43 +109,62 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
         }
     }
 
+    // Callback para manejar la respuesta del usuario a la solicitud de permisos
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         when (requestCode) {
-            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                map.isMyLocationEnabled = true
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Para activar la localizacion ve a ajustes y acepta los permisos",
-                    Toast.LENGTH_SHORT
-                ).show()
+            REQUEST_CODE_LOCATION -> {
+                // Verifica si el usuario concedió los permisos
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Si se concedieron, habilita la capa de ubicación en tiempo real del mapa
+                    map.isMyLocationEnabled = true
+                } else {
+                    // Si se denegaron, muestra un mensaje al usuario
+                    Toast.makeText(
+                        requireContext(),
+                        "Para activar la localización ve a ajustes y acepta los permisos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
 
+    // Callback para manejar la resolución al reanudar la aplicación
     override fun onResume() {
         super.onResume()
         if (!::map.isInitialized) return
+        // Verifica si los permisos de ubicación están activados
         if (!isLocationPermissionGranted()) {
+            // Si no están activados, desactiva la capa de ubicación en tiempo real del mapa
             map.isMyLocationEnabled = false
+            // Muestra un mensaje al usuario
             Toast.makeText(
                 requireContext(),
-                "Para activar la localización ve a ajustes y acepta los permisos",
+                "Activa tu ubicación en ajustes",
                 Toast.LENGTH_SHORT
             ).show()
         }
     }
 
+    // Verifica si la ubicación está activada al reanudar la aplicación
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
 
+    // Callback para manejar el clic en el botón de ubicación en tiempo real
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(requireContext(), "Boton pulsado", Toast.LENGTH_SHORT).show()
         return false
     }
 
+    // Callback para manejar el clic en el mapa
     override fun onMapClick(p0: LatLng) {
         Toast.makeText(requireContext(), "Estas en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
