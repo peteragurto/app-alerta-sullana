de794d9362dc428adcafcda0b4279610762a4dfa
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-12-28 23:59:20 -0500

    Añade buildconfig y actualiza librerías

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,23 +39,24 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
 
-    val cameraVersion = "1.3.2"
-    val navVersion = "2.7.7"
-    val hiltVersion = "2.48"
+    val cameraVersion = "1.4.1"
+    val navVersion = "2.8.5"
+    val hiltVersion = "2.51"
 
     //Base
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //Navegación
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
