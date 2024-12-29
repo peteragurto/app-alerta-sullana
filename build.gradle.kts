97525bf0b9467600dcf8b4f182f4d6bdbc0afdad
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-12-29 00:00:25 -0500

    Añade AGP y versión de Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
